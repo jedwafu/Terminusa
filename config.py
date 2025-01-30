d727db17c16d485d960e3a0a9354a2ea3205f534
Joedel Lopez Dalioan <joedel.dalioan@yahoo.com>
2025-01-30 11:12:09 +0800

    Reverted
    Back to legacy.

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class TerminusaConfig:
-    def __init__(self):
-        self.TAC_MAX = 10**10
-        self.KOII_RATE = 100
-        self.INITIAL_TAC = 100
-        self.NODE_ENDPOINT = os.getenv('KOII_NODE', 'https://node.koii.network')
